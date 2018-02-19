6a96b55d15a060e5776fba9310dccb496fb96bb9
William <william_y_abboud@abv.bg>
2018-02-19 22:53:34 +0200

    Extended App component to fetch public photos

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import flickrApi from './flickr-service';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      photos: []
+    };
+  }
+
+  componentDidMount() {
+    flickrApi.getPublicPhotos()
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({ photos: data });
+      });
   }
 
   render() {
-    return (
-      <div>{ 'Hello World' }</div>
-    );
+    return <div>Hello World</div>;
   }
 }
 
