38a9e0f72714001441896a2f4e74cb93ec1da39b
William <william_y_abboud@abv.bg>
2018-02-19 22:12:14 +0200

    Added tests for PhotoWithDetails component

---
+++ src/test/PhotoWithDetails.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import PhotoWithDetails from '../js/PhotoWithDetails';
+
+describe("Photo with details component", () => {
+  const props = {
+    photoURL: "some-url-string",
+    photoLink: "some-photo-link",
+    authorLink: "some-author-link",
+    title: "Some title",
+    description: "This is a fake description",
+    tags: ["tag one", "tag two", "tag three"],
+  };
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<PhotoWithDetails {...props} />);
+  });
+
+  it("shows an image based on supplied image url", () => {
+    expect(wrapper.contains(<img src={props.photoURL} />)).toEqual(true);
+  });
+
+  it("shows title based on supplied title", () => {
+    expect(wrapper.text().indexOf(props.title)).not.toBe(-1);
+  });
+
+  it("shows description", () => {
+    expect(wrapper.text().indexOf(props.description)).not.toBe(-1);
+  });
+
+  it("shows tags", () => {
+    const text = wrapper.text();
+    const isTagShown = tag => expect(text.indexOf(tag)).not.toBe(-1);
+
+    props.tags.forEach(isTagShown);
+  });
+
+  it("provides link to author", () => {
+    expect(wrapper.find(`[href="${props.authorLink}"]`)).toHaveLength(1);
+  });
+
+  it("provides link to image", () => {
+    expect(wrapper.find(`[href="${props.photoLink}"]`)).toHaveLength(1);
+  });
+});
