47641105910e1154705028d482a3ed81508f1547
William <william_y_abboud@abv.bg>
2018-02-20 19:49:08 +0200

    Added classes for styling and improved logic inside component

--- src/js/PhotoWithDetails.jsx
+++ src/js/PhotoWithDetails.jsx
@@ -1,35 +1,85 @@
 import React from 'react';
-import { string, arrayOf, oneOfType } from 'prop-types';
+import { string, bool, arrayOf, oneOfType } from 'prop-types';
+import Collapsible from 'react-collapsible';
+
+function Tag({ children }) {
+  if (!children.trim().length) {
+    return null;
+  }
+
+  return <span className="tag">{children}</span>;
+}
+
+Tag.propTypes = {
+  children: string.isRequired
+};
 
 function PhotoWithDetails({
   photoURL,
   title,
   description,
+  hideDescription,
   authorLink,
+  authorName,
   photoLink,
   tags
 }) {
   return (
-    <div>
-      <img src={photoURL} alt={description} />
-      <h2>{title}</h2>
-      <a href={authorLink}>author</a>
-      <a href={photoLink}>photo link</a>
-      <p>{description}</p>
-      <p>
-        {tags.map(tag => <span key={tag}>{ tag }</span>)}
-      </p>
+    <div className="photo-with-details">
+      <img className="photo" src={photoURL} alt={title} />
+      <div className="title-author-container">
+        <h3 className="title">
+          <a className="photo-link" href={photoLink}>
+            { title.trim().length ? title : "Untitled" }
+          </a>
+        </h3>
+        <span>by</span>
+        <span className="author">author: {authorName}</span>
+      </div>
+      {
+        (hideDescription)
+          ?
+            <Collapsible
+              transitionTime={300}
+              trigger={
+                <button className="description-trigger">See more</button>
+              }
+            >
+              <p className="description" dangerouslySetInnerHTML={{ __html: description }} />
+            </Collapsible>
+          :
+            <p className="description" dangerouslySetInnerHTML={{ __html: description }} />
+      }
+      {
+        (tags.length)
+          ?
+            <p className="tags">
+              {
+                Array.isArray(tags)
+                  ? tags.map(tag => <Tag key={tag}>{tag}</Tag>)
+                  : tags.split(" ").map(tag => <Tag key={tag}>{tag}</Tag>)
+              }
+            </p>
+          :
+            null
+      }
     </div>
   );
 }
 
 PhotoWithDetails.propTypes = {
   photoURL: string.isRequired,
-  title: string.isRequired,
-  authorLink: string.isRequired,
+  title: string,
+  authorName: string.isRequired,
   photoLink: string.isRequired,
   description: string.isRequired,
-  tags: oneOfType([ string, arrayOf(string) ]).isRequired
+  tags: oneOfType([ string, arrayOf(string) ]).isRequired,
+  hideDescription: bool,
+};
+
+PhotoWithDetails.defaultProps = {
+  hideDescription: false,
+  title: "Untitled",
 };
 
 export default PhotoWithDetails;
